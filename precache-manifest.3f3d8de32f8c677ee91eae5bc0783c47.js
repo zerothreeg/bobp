bcf0ecc330a12a3f31ea228b1d783708df2792a4
AplinXy9plin <evilberry@icloud.com>
2019-08-21 15:38:46 +0700

    Updates

---
+++ precache-manifest.3f3d8de32f8c677ee91eae5bc0783c47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c30a258e37f4ec18b4b8",
+    "url": "/BuyApp-HS/static/js/runtime~main.10cc8f58.js"
+  },
+  {
+    "revision": "09a834a78f9b9f9ac30d",
+    "url": "/BuyApp-HS/static/js/main.10228f3c.chunk.js"
+  },
+  {
+    "revision": "2c245c6ccea9c25ebcfb",
+    "url": "/BuyApp-HS/static/js/2.79102661.chunk.js"
+  },
+  {
+    "revision": "2c245c6ccea9c25ebcfb",
+    "url": "/BuyApp-HS/static/css/2.c81a22d2.chunk.css"
+  },
+  {
+    "revision": "e049fd32b25006d60644626cd834bacc",
+    "url": "/BuyApp-HS/index.html"
+  }
+];
